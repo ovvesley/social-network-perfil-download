f500a924d309ec4a1805c3e6dd114bd588daf0ea
Wesley Ferreira <wsf.ley@gmail.com>
2019-04-20 21:11:12 -0300

    request inicias

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
-r = requests.get('https://www.google.com')
+import json
+#username = raw_input('Digite o perfil do usuario')
+username = 'john'
 
-
+r = requests.get('https://facebook.com/' + username + '/photos')
+s = requests.Session()
+s.post('https://facebook.com/' + username + '/photos', acessKey)
+print(r.url)
 soup = BeautifulSoup(r.content, 'html.parser')
-print(soup.prettify())
+
+
+fotos = soup.find_all(class_= 'uiMediaThumbImg')
+print(fotos)
+for itema in fotos:
+    print(itema)
+
+
+    
+#print(lista)
+
+#print(soup.prettify())
